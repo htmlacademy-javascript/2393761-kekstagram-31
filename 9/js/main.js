65ba5a1e95b2b29b94c0062eacc5cea55dc2968a
Keks <keks@htmlacademy.ru>
2024-03-26 00:52:54 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,8 @@
+
+
+import { renderingThumbnails } from './showMePhotos.js';
+import { openModalBigPicture } from './renderPhoto.js';
+import './form.js';
+
+renderingThumbnails();
+openModalBigPicture();
