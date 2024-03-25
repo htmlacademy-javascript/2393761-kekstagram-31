c90fc13b9d6a8da1f0e58885735c3cfa73d5032c
ulas-mikhail <145497146+ulas-mikhail@users.noreply.github.com>
2024-03-26 00:49:39 +0300

    готовое задание правда или действие module9-task1

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 import { renderingThumbnails } from './showMePhotos.js';
 import { openModalBigPicture } from './renderPhoto.js';
-
+import './form.js';
 
 renderingThumbnails();
 openModalBigPicture();
